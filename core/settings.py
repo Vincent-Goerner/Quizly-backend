f79c73c83c89c35941e432a9222e8a836eadca18
VincentGörner <161941709+A1exi0sD4rc@users.noreply.github.com>
2025-12-12 18:37:33 +0100

    add allowed_hosts to settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-40)7o1vz%l9+-9)x+@nj3se@i4lm1ty&s-pfro98)l!xi^-f@u'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DEBUG=True
 
